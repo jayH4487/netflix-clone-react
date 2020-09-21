5a68b4050ebe72a7f36a89f36c78fc8b672ad7b5
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-09-21 15:07:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <p>netflix app</p>
+                </Route>
+                <Route path="/signin">
+                    <p>Sign in Page</p>
+                </Route>
+                <Route path="/signup">
+                    <p>Sign up Page</p>
+                </Route>
+                <Route path="/browse">
+                    <p>Browse Page</p>
+                </Route>
+            </Switch>
+        </Router>
+    )
 }
 
-export default App;
+export default App
