07279d99d72fdda29c925210c342feea4d068f54
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-09-22 10:09:43 +0100

    global styles added

---
+++ src/global-styles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components"
+
+export const GlobalStyles = createGlobalStyle`
+    html,
+    body {
+        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+        background: black;
+        color: #333333;
+        font-size: 16px;
+        margin: 0;
+    }
+`
