3711c4f8ce35eda794655f18fb1a12f5fc94116b
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-09-27 16:59:40 +0100

    routes

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import Home from "./pages/home"
+import * as ROUTES from "./constants/routes"
 
 function App() {
     return (
         <Router>
             <Switch>
-                <Route exact path="/">
+                <Route exact path={ROUTES.HOME}>
                     <Home />
                 </Route>
-                <Route path="/signin">
+                <Route path={ROUTES.SIGN_IN}>
                     <p>Sign in Page</p>
                 </Route>
-                <Route path="/signup">
+                <Route path={ROUTES.SIGN_UP}>
                     <p>Sign up Page</p>
                 </Route>
-                <Route path="/browse">
+                <Route path={ROUTES.BROWSE}>
                     <p>Browse Page</p>
                 </Route>
             </Switch>
