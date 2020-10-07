3abf0c749caa4abc22cf678b5ce526ee7f2d1dbf
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-10-07 17:32:00 +0100

    live search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState, useEffect } from "react"
+import Fuse from "fuse.js"
 
 import { Header, Loading, Card } from "../components"
 import * as ROUTES from "../constants/routes"
@@ -31,6 +32,18 @@ export function BrowseContainer({ slides }) {
     useEffect(() => {
         setSlideRows(slides[category])
     }, [slides, category])
+
+    useEffect(() => {
+        const fuse = new Fuse(slideRows, { keys: ["data.description", "data.title", "data.genre"] })
+        const results = fuse.search(searchTerm).map(({ item }) => item)
+
+        if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+            setSlideRows(results)
+        } else {
+            setSlideRows(slides[category])
+        }
+        
+    }, [searchTerm])
     
 
     return (
