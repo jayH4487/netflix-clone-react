0e581ee31ff9775d995170280f02b872324e68ad
jayH4487 <59086697+jayH4487@users.noreply.github.com>
2020-10-15 13:44:23 +0100

    Updates

---
+++ precache-manifest.fe611cc8ce9a73d1a02751c14e373420.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbf56f46b48f3d869823647af2789c9",
+    "url": "/netflix-clone-react/index.html"
+  },
+  {
+    "revision": "04dcf7f3201069991254",
+    "url": "/netflix-clone-react/static/js/2.16fc1d68.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/netflix-clone-react/static/js/2.16fc1d68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cfc94edce359ad03e15",
+    "url": "/netflix-clone-react/static/js/main.129e8452.chunk.js"
+  },
+  {
+    "revision": "eadf63e2318c50f407ef",
+    "url": "/netflix-clone-react/static/js/runtime-main.c7b1a352.js"
+  }
+]);
